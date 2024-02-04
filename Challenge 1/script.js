3a253df56d400efdd29fa25d15782d6f5c4b6c41
eric <ericjnr.ed@gmail.com>
2024-02-05 00:07:00 +0200

    client info import and export

---
+++ Challenge 1/script.js
@@ -0,0 +1,9 @@
+// scripts.js
+
+import {company, year} from './configuration';
+
+document.querySelector('footer').innerText = message;
+
+    const message = '© ' + company + ' (' + year + ')';
+
+console.log(message);
---
+++ Challenge 2/alex.js
@@ -0,0 +1,8 @@
+// alex.js
+
+const firstname = "alex";
+const surname = "Naidoo";
+const role = "Head of Marketing";
+
+const display = (firstname, surname, role);
+document.querySelector('#alex').innerTEXT = display; 
