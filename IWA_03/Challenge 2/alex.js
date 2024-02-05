1bedeea652e06839d80e9061501c2222446a09bf
Eric Diswane <148723274+ericjnr08@users.noreply.github.com>
2024-02-05 22:55:39 +0200

    Final update
    Cleared out code and restarted after coaching season

---
+++ IWA_03/Challenge 2/alex.js
@@ -0,0 +1,11 @@
+// alex.js
+
+export const firstname = "Alex";
+export const surname = "Naidoo";
+export const role = "Head of Marketing";
+
+const display = `${firstname} ${role}`
+document.querySelector('#alex').innerText = display
+
+
+/*document.querySelector('[data-key="#alex"]').innerTEXT = display; */
---
+++ IWA_03/Challenge 2/scripts.js
@@ -0,0 +1,7 @@
+// scripts.js
+
+import { role as alex } from './alex.js'
+import { role as johannes } from './johannes.js'
+import { role as nwabisa } from './nwabisa.js'
+
+console.log('Roles:', nwabisa, johannes, alex)
