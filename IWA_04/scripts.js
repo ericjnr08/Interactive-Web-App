6ecc80776d696407f6fd912aadb0bd43ae14c1dd
Eric Diswane <148723274+ericjnr08@users.noreply.github.com>
2024-02-06 23:39:00 +0200

    Console querry
    Code has two variations and should be seen for in yow the look

---
+++ IWA_04/scripts.js
@@ -0,0 +1,53 @@
+let stauts = "parent";
+let year = 2050 
+let count = 9
+let date = "January"
+
+
+if (date === "January") {
+  console.log("January is:", 'New Year’s Day');
+
+if (date === "March") {
+  console.log("March is:", 'Human Rights Day');
+
+}
+ 
+if (date === "April") {
+  console.log(date, 'Family Day');
+}
+  
+console.log(date, 'Freedom Day');
+  count += 4;
+
+let status = "student";
+
+  if (status === "student") {
+    console.log('June', 'Youth Day');
+  }
+
+if (date === "August") {
+  connsole.log("August is:", "Women's Day");
+
+}
+
+if (date === "September") {
+  connsole.log("September is:", "Heritage Day");
+
+}
+  
+let date = 'December';
+  console.log(date, 'Day of Reconciliation');
+
+
+if (status === "parent") {
+    console.log(date, 'Christmas Day');
+
+  }
+
+  console.log(date, 'Day of Goodwill');
+  count += 1;
+}
+
+console.log('Your status is:', status);
+console.log('The year is:', year);
+console.log('The total holidays is:', count);
