f093972e8c8bd4e0f6f41af50f399756c3b970c0
Eric Diswane <148723274+ericjnr08@users.noreply.github.com>
2024-03-18 22:56:21 +0200

    Deconstruction and coaching update.

--- IWA_15/challenge 1.js
+++ IWA_15/challenge 1.js
@@ -1,48 +1,47 @@
 // scripts.js
 
 const data = {
-	lists: [
-		['first', [15, 11, 13, 7, 5]],
-		['second', [2, 6, 8, 4, 14, 12, 10]],
-		['third', [9, 3, 1]],
-	]
+    lists: [
+        ['first', [15, 11, 13, 7, 5]],
+        ['second', [2, 6, 8, 4, 14, 12, 10]],
+        ['third', [9, 3, 1]],
+    ]
 }
+
 // Only edit below
 
 /**
- * Adding all the obj numbers into one array 'result'
- * making use of 'reduce' I pass directly through the array and
- * adding into two values (label and numbers)
+ * Firstly log the data by deconstructing the code.
+ * we skip the numbers and just take the word (right to left)
  */
-const result = data.lists.reduce((acc,[, numbers]) => {
-	return acc.concat(numbers);
-},[]);
-console.log(result);
+const {
+    lists: [
+        [, first],
+        [, second],
+        [, third],
+    ]
+} = data
+
+const result = []
 
 const extractBiggest = () => {
-	const biggestNumber = []
-	for(const [,number] of data.lists) {
-		const biggest = number[number.length - 1]; //pull the last item
-		biggestNumber.push(biggest);
-		number.pop(); //removing the last item from the array
-	}
-	return biggestNumber;
+  const firstLast = first.at(-1)
+    const secondLast = second.at(-1)
+    const thirdLast = third.at(-1)
+
+    if (firstLast > secondLast && firstLast > thirdLast) {
+        first.pop()
+        return firstLast
+    }
+
+    if (secondLast > thirdLast) {
+        second.pop()
+        return secondLast
+    }
+
+    third.pop()
+    return thirdLast
 }
-
-// const extractBiggest = () => {
-//     //const maxValues = data.lists
-	
-//    //remove negative symbole 
-//     if (first[1] > second[1]) {
-// 		return first
-// 	}
-
-// 	if (third[1] < 1) {
-// 		return second
-// 	}
-	
-// 	return third
-// }
 
 // Only edit above
 
